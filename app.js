6d11d8874930baa59c39bb641962d622f2dc1c2a
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-06-20 09:19:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+alert('Hello Word')
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let numberOne = 68
+let numberTwo = 23
+
+let sum = numberOne + numberTwo
+
+console.log(sum)
+
+//Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+const isNumber = 89;
+
+if (typeof isNumber === 'number') {
+  console.log('É um número')
+} else {
+  console.log('Não é um número')
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+const isString = 'marcia'
+
+if (typeof isString === 'string') {
+  console.log("É uma String")
+} else {
+  console.log("Não é uma String")
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um booleano.Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+const isBoolean = true
+
+if (typeof isBoolean == 'boolean') {
+  console.log('É um Boolean')
+} else {
+  console.log('Não é um Boolean')
+}
+
+//Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+const firstNumber = 190
+const secondNumber = 345
+
+const subtraction = firstNumber - secondNumber
+
+console.log(subtraction)
+
+// Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+const n1 = 46
+const n2 = 90
+const multiply = n1 * n1
+
+console.log(multiply)
+
+//Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+const n3 = 290
+const n4 = 20
+const division = n3 / n4
+
+console.log(division)
+
+//Crie um script que declare uma variável e verifique se o seu valor é um número par.Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par"
+
+const evenNumber = 100
+if (evenNumber % 2 === 0) {
+  console.log('É um número par')
+} else {
+  console.log('Não é um número par')
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+const oddNumber = 35
+if (oddNumber % 2 !== 0) {
+  console.log('É um número ímpar')
+} else {
+  console.log('Não é um número ímpar')
+}
